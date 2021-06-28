217f0dfc715edd32493c06bf1069bbfcb48186b8
zhukewen <stevekewen@163.com>
2021-06-28 17:17:04 +0800

    update

--- quicksort.py
+++ quicksort.py
@@ -6,15 +6,16 @@ nums = [23,2,4,6,2,5,1,6,13,54,8]
 def quicksort(nums, left, right):
     if left >= right:
         return
+    pivot = left
     i, j = left, right
     while i < j:
-        while nums[left] <= nums[j] and i < j:
+        while nums[pivot] <= nums[j] and i < j:
             j -= 1
-        while nums[left] >= nums[i] and i < j:
+        while nums[pivot] >= nums[i] and i < j:
             i += 1
         if i < j:
             nums[i], nums[j] = nums[j], nums[i]
-    nums[left], nums[i] = nums[i], nums[left]
+    nums[pivot], nums[i] = nums[i], nums[pivot]
     quicksort(nums, left, i-1)
     quicksort(nums, i+1, right)
 
@@ -25,15 +26,16 @@ def quicksort2(nums, left, right):          # 用栈代替递归
     stack = []
     while stack or left < right:
         if left < right:
+            pivot = left
             i, j = left, right
             while i < j:
-                while nums[left] <= nums[j] and i < j:
+                while nums[pivot] <= nums[j] and i < j:
                     j -= 1
-                while nums[left] >= nums[i] and i < j:
+                while nums[pivot] >= nums[i] and i < j:
                     i += 1
                 if i < j:
                     nums[i], nums[j] = nums[j], nums[i]
-            nums[left], nums[i] = nums[i], nums[left]
+            nums[pivot], nums[i] = nums[i], nums[pivot]
             stack.append((left, i, right))
             right = i - 1
         else:
@@ -45,3 +47,44 @@ print(nums)
 
 # ------------------------快速排序的优化----------------------------
 # ------------------------A三路快速排序-----------------------------
+def quicksort_opt1(nums, left, right): 
+    if left >= right:
+        return
+    stack = []
+    while stack or left < right:
+        if left < right:
+            l, m, r = left, (right-left)//2, right # 第一个, 中间位置, 最后一个
+            pivot = findmedian(l, m, r) # 三数取中值法选取枢轴元pivot
+            i, j = left, right
+            while i < j:
+                while nums[pivot] <= nums[j] and i < j:
+                    j -= 1
+                while nums[pivot] >= nums[i] and i < j:
+                    i += 1
+                if i < j:
+                    nums[i], nums[j] = nums[j], nums[i]
+            nums[pivot], nums[i] = nums[i], nums[pivot]
+            stack.append((left, i, right))
+            right = i - 1
+        else:
+            left, mid, right = stack.pop()
+            left = mid + 1
+
+def findmedian(l, m, r):
+    if nums[l] <= nums[m]:
+        if nums[m] <= nums[r]:
+            return m
+        else:
+            if nums[l] >= nums[r]:
+                return l
+            else:
+                return r
+    else:
+        if nums[m] >= nums[r]:
+            return m
+        else:
+            if nums[l] >= nums[r]:
+                return r
+            else:
+                return l
+
